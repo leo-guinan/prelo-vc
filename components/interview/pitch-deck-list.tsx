2809587910af17b8897a25fd3fcd792441bd96d0
Leo Guinan <leo.guinan@gmail.com>
2024-07-28 13:11:09 -0400

    cleaning imports

--- components/interview/pitch-deck-list.tsx
+++ components/interview/pitch-deck-list.tsx
@@ -1,10 +1,6 @@
 'use client'
-import React, { useState, useEffect } from 'react';
-import { BarChart, RefreshCcw, FileText } from 'lucide-react';
-import {PitchDeck, PitchDeckProcessingStatus} from "@prisma/client/edge";
-import ProgressBar from "@/components/progress-bar";
-import { getDeck } from '@/app/actions/interview';
-import { useRouter } from 'next/navigation';
+import React from 'react';
+import {PitchDeck} from "@prisma/client/edge";
 import ViewPitchDeck from "@/components/interview/pitch-deck";
 
 interface PitchDeckListProps {
@@ -15,17 +11,17 @@ interface PitchDeckListProps {
 export default function PitchDeckList({decks}: PitchDeckListProps) {
 
 
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Uploaded Decks</h2>
-      <ul className="space-y-4">
-        {decks?.map((deck) => (
-          <li key={deck.id} className="flex items-center justify-between p-4 rounded-lg">
-            <ViewPitchDeck deck={deck} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="p-4">
+            <h2 className="text-2xl font-bold mb-4">Uploaded Decks</h2>
+            <ul className="space-y-4">
+                {decks?.map((deck) => (
+                    <li key={deck.id} className="flex items-center justify-between p-4 rounded-lg">
+                        <ViewPitchDeck deck={deck}/>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
