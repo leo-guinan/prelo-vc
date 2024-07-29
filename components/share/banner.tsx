f35638a0ec6e27feddc119477c3a8a5e4810377a
Leo Guinan <leo.guinan@gmail.com>
2024-07-29 11:44:24 -0400

    fixing ui of share page

--- components/share/banner.tsx
+++ components/share/banner.tsx
@@ -1,12 +1,6 @@
-import Link from "next/link";
 
-interface BannerProps {
-    deckUUID: string
-    reportUUID: string
 
-}
-
-export async function Banner({deckUUID, reportUUID}: BannerProps) {
+export async function Banner() {
 
     return (
 
@@ -14,11 +8,8 @@ export async function Banner({deckUUID, reportUUID}: BannerProps) {
             <h1 className="text-3xl font-bold sm:text-4xl flex items-center sm:justify-start flex-col sm:flex-row mx-auto sm:mx-0">
                 <span
                     className="text-standard mr-4">
-                        Want to see more details of this deck?
-                    <Link href={`/login?redirect=/view/${deckUUID}/${reportUUID}`}
-                          className="text-zinc-50 underline cursor-pointer pl-2">
-                        Sign up for a free account!
-                    </Link>
+                        Register to learn more about this deal
+
             </span>
             </h1>
         </div>
--- components/share/shared-report.tsx
+++ components/share/shared-report.tsx
@@ -1,7 +1,10 @@
 import {ScrollArea} from "@/components/ui/scroll-area";
 import MarkdownBlock from "@/components/ui/markdown-block";
 import {Banner} from "@/components/share/banner";
-import { auth } from "@/auth";
+import {auth} from "@/auth";
+import Link from "next/link";
+import {Button} from "../ui/button";
+import {IconShare} from "@/components/ui/icons";
 
 interface SharedReportProps {
     report: string
@@ -15,7 +18,7 @@ export default async function SharedReport({report, company_name, deckUUID, repo
     const session = await auth()
     return (
         <div className="relative">
-            {!session?.user && <Banner deckUUID={deckUUID} reportUUID={reportUUID}/>}
+            {!session?.user && <Banner/>}
 
             <div className="container w-full max-w-3xl mx-auto mt-24">
                 <div className={'pt-4 md:pt-10 size-full mx-auto box-border'}>
@@ -26,9 +29,23 @@ export default async function SharedReport({report, company_name, deckUUID, repo
                             <MarkdownBlock content={report}/>
                         </ScrollArea>
 
-
                     </div>
-
+                    <div className="flex flex-row space-x-4">
+                        {!session?.user && (
+                            <Link href={`/login?redirect=/view/${deckUUID}/${reportUUID}`}
+                            >
+                                <Button
+                                    className="rounded bg-objections px-6 py-3 h-10 text-sm font-semibold text-standard shadow-sm hover:bg-amber-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">
+                                    Create Account
+                                </Button>
+                            </Link>
+                        )}
+                        <Button type="button" variant="default"
+                                className="rounded bg-indigo-600 px-6 py-3 h-10 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">
+                            <IconShare className="mr-2"/>
+                            <span>Share</span>
+                        </Button>
+                    </div>
 
                 </div>
             </div>
