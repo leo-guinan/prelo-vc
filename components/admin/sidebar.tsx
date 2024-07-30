a52a2a78b5e39ea1b3d3e5f477a35c13d08b68b9
Leo Guinan <leo.guinan@gmail.com>
2024-07-30 15:00:37 -0400

    adding to admin too

--- components/admin/sidebar.tsx
+++ components/admin/sidebar.tsx
@@ -6,6 +6,7 @@ import {Sidebar} from "@/components/sidebar";
 import {AnalysisSidebar} from "@/components/analysis-sidebar";
 import useSwr, {useSWRConfig} from "swr";
 import {useRouter} from "next/navigation";
+import {DeckSidebar} from "@/components/deck-sidebar";
 
 interface AdminSidebarProps {
     users: User[]
@@ -18,6 +19,7 @@ export default function AdminSidebar({users}: AdminSidebarProps) {
     const router = useRouter()
 
     const handleSelect = async (item: {id: string, name: string}) => {
+        setSelectedUserId(item.id)
         router.push(`/admin/interview/${item.id}`)
     }
 
@@ -26,6 +28,7 @@ export default function AdminSidebar({users}: AdminSidebarProps) {
             <Sidebar
                 className="peer absolute inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
                 <SearchableDropdown items={displayedUsers} onSelect={handleSelect}/>
+                <DeckSidebar userId={selectedUserId}/>
             </Sidebar>
 
         </>
