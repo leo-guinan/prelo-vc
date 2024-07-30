1283e42eaf040e0745b3d15f3f5454aab5710c1e
Leo Guinan <leo.guinan@gmail.com>
2024-07-30 09:59:17 -0400

    fixing share page

--- app/(share)/share/[deck_uuid]/[report_uuid]/page.tsx
+++ app/(share)/share/[deck_uuid]/[report_uuid]/page.tsx
@@ -1,5 +1,7 @@
 import {getSharedReport} from "@/app/actions/share";
+import { auth } from "@/auth";
 import SharedReport from "@/components/share/shared-report";
+import { User } from "@prisma/client/edge";
 
 interface PitchDeckPageProps {
     params: {
@@ -11,7 +13,7 @@ interface PitchDeckPageProps {
 export default async function SharedDeckReportPage({params}: PitchDeckPageProps) {
 
     const {executive_summary, company_name} = await getSharedReport(params.deck_uuid, params.report_uuid)
-
+    const session = await auth()
     return <SharedReport report={executive_summary} company_name={company_name} reportUUID={params.report_uuid}
-                         deckUUID={params.deck_uuid}/>
+                         deckUUID={params.deck_uuid} user={session?.user as User}/>
 }
--- components/share/shared-report.tsx
+++ components/share/shared-report.tsx
@@ -1,37 +1,57 @@
+'use client';
 import {ScrollArea} from "@/components/ui/scroll-area";
 import MarkdownBlock from "@/components/ui/markdown-block";
 import {Banner} from "@/components/share/banner";
 import {auth} from "@/auth";
 import Link from "next/link";
-import {Button} from "../ui/button";
-import {IconShare} from "@/components/ui/icons";
+import {Button, buttonVariants} from "../ui/button";
+import {IconCheck, IconCopy, IconShare} from "@/components/ui/icons";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog";
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
+import {cn} from "@/lib/utils";
+import {useCopyToClipboard} from "@/lib/hooks/use-copy-to-clipboard";
+import { User } from "@prisma/client/edge";
 
 interface SharedReportProps {
     report: string
     company_name: string
     reportUUID: string
     deckUUID: string
+    user?: User
 
 }
 
-export default async function SharedReport({report, company_name, deckUUID, reportUUID}: SharedReportProps) {
-    const session = await auth()
+export default function SharedReport({report, company_name, deckUUID, reportUUID, user}: SharedReportProps) {
+    const copyShareLink = async () => {
+        if (isCopied) return
+        copyToClipboard(`${window.location.origin}/share/${deckUUID}/${reportUUID}`)
+    }
+    const {isCopied, copyToClipboard} = useCopyToClipboard({timeout: 2000})
+
     return (
         <div className="relative">
-            {!session?.user && <Banner/>}
+            {!user && <Banner/>}
 
             <div className="container w-full max-w-3xl mx-auto mt-24">
                 <div className={'pt-4 md:pt-10 size-full mx-auto box-border'}>
 
                     <div className="flex flex-col-reverse sm:flex-row h-full">
-                        <h1 className="text-3xl font-bold text-center mb-8">{company_name}</h1>
                         <ScrollArea className="flex flex-col size-full pb-8">
                             <MarkdownBlock content={report}/>
                         </ScrollArea>
 
                     </div>
                     <div className="flex flex-row space-x-4">
-                        {!session?.user && (
+                        {!user && (
                             <Link href={`/login?redirect=/view/${deckUUID}/${reportUUID}`}
                             >
                                 <Button
@@ -40,11 +60,59 @@ export default async function SharedReport({report, company_name, deckUUID, repo
                                 </Button>
                             </Link>
                         )}
-                        <Button type="button" variant="default"
-                                className="rounded bg-indigo-600 px-6 py-3 h-10 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">
-                            <IconShare className="mr-2"/>
-                            <span>Share</span>
-                        </Button>
+                        <Dialog>
+                            <DialogTrigger>
+                                <>
+                                    <TooltipProvider>
+                                        <Tooltip>
+                                            <TooltipTrigger asChild>
+                                                <Button type="button" variant="default"
+                                                        className="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                                    <IconShare className="mr-2"/>
+                                                    <span>Share</span>
+                                                </Button>
+                                            </TooltipTrigger>
+                                            <TooltipContent className="max-w-xs">
+                                                <p className="break-words">Share with another investor</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+
+                                </>
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Share This Deal</DialogTitle>
+                                    <DialogDescription>
+
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <div className="h-96 overflow-y-scroll">
+                                    <ScrollArea>
+                                        <MarkdownBlock content={report}/>
+                                    </ScrollArea>
+                                </div>
+                                <DialogFooter className="sm:justify-start">
+
+                                    <DialogClose asChild>
+                                        <Button type="button" variant="secondary" className="p-4 h-10">
+                                            Close
+                                        </Button>
+                                    </DialogClose>
+                                    <Link
+                                        onClick={copyShareLink}
+                                        href="#"
+                                        className={cn(
+                                            buttonVariants({variant: 'outline'}),
+                                            'h-10 text-zinc-50 dark:text-gray-900 justify-start bg-standard dark:bg-gray-100 p-4 shadow-none transition-colors hover:bg-gray-100 hover:text-gray-900  dark:hover:bg-standard dark:hover:text-zinc-50'
+                                        )}
+                                    >
+                                        {isCopied ? <IconCheck/> : <IconCopy/>}
+                                        Copy Share Link
+                                    </Link>
+                                </DialogFooter>
+                            </DialogContent>
+                        </Dialog>
                     </div>
 
                 </div>
