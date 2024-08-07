a4c5ebd7c0a8175603876b120917d5846ce55e07
Leo Guinan <leo.guinan@gmail.com>
2024-08-07 16:58:04 -0400

    adding user notifications to slack + loops email list handling

--- auth.ts
+++ auth.ts
@@ -1,4 +1,4 @@
-import {PrismaClient} from '@prisma/client/edge'
+import {PrismaClient, User} from '@prisma/client/edge'
 import {PrismaAdapter} from "@auth/prisma-adapter";
 import NextAuth, {type DefaultSession} from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
@@ -37,16 +37,132 @@ export const authOptions = {
             return token
         },
         // @ts-ignore
-        session: ({session, token}) => {
-            if (session?.user && token?.id) {
+        session: async ({session, token}) => {
+            const user = session?.user as User
+
+            if (user && token?.id) {
                 session.user.id = String(token.id)
             }
+
+            if (user && !user.loopsId) {
+                await fetch("https://n8n-e50h.onrender.com/webhook-test/85ed86de-07ad-4e14-a5f8-3f202f30eb27", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        name: user.name,
+                        email: user.email,
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                try {
+
+                    const userOnListOptions = {
+                        method: 'GET',
+                        headers: {Authorization: `Bearer ${process.env.LOOPS_API_KEY}`}
+                    };
+
+                    const userOnListResponse = await fetch(`https://app.loops.so/api/v1/contacts/find?email=${user.email}`, userOnListOptions)
+                    const userOnList = await userOnListResponse.json()
+                    if (userOnList.length > 0) {
+                        await prisma.user.update({
+                            where: {
+                                id: user.id
+                            },
+                            data: {
+                                loopsId: userOnList[0].id
+                            }
+                        })
+                        const eventOptions = {
+                            method: 'POST',
+                            headers: {
+                                Authorization: `Bearer ${process.env.LOOPS_API_KEY}`,
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                "email": user.email,
+                                "userId": user.id,
+                                "eventName": "userSignup",
+                                "eventProperties": {
+                                    "app": "PreloVC"
+                                }
+                            })
+                        };
+                        // eventually need to handle case when this fails, but for now, don't worry about it
+                        const eventAnswer = await fetch('https://app.loops.so/api/v1/events/send', eventOptions)
+                        const eventAnswerJson = await eventAnswer.json()
+
+
+                        return session
+                    }
+
+                    const options = {
+                        method: 'POST',
+                        headers: {
+                            Authorization: `Bearer ${process.env.LOOPS_API_KEY}`,
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(
+                            {
+                                email: user.email,
+                                name: user.name,
+                                source: "API",
+                                subscribed: true,
+                                userGroup: "PreloVC Users",
+                                userId: user.id
+                            }
+                        )
+                    };
+
+                    const result = await fetch('https://app.loops.so/api/v1/contacts/create', options)
+
+                    const answer = await result.json()
+                    if (!answer.success) {
+                        console.error("Error adding user to loops campaign", answer)
+                    } else {
+                        await prisma.user.update({
+                            where: {
+                                id: user.id
+                            },
+                            data: {
+                                loopsId: answer.id
+                            }
+                        })
+                        const eventOptions = {
+                            method: 'POST',
+                            headers: {
+                                Authorization: `Bearer ${process.env.LOOPS_API_KEY}`,
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                                "email": user.email,
+                                "userId": user.id,
+                                "eventName": "userSignup",
+                                "eventProperties": {
+                                    "app": "PreloVC"
+                                }
+                            })
+                        };
+                        // eventually need to handle case when this fails, but for now, don't worry about it
+                        const eventAnswer = await fetch('https://app.loops.so/api/v1/events/send', eventOptions)
+                        const eventAnswerJson = await eventAnswer.json()
+
+                    }
+                } catch (e) {
+                    console.error("Error adding user to loops campaign", e)
+                }
+
+
+            }
+
             return session
-        },
+        }
+        ,
         // @ts-ignore
         authorized({auth}) {
             return !!auth?.user // this ensures there is a logged in user for -every- request
-        },
+        }
+        ,
 
     },
     pages: {
