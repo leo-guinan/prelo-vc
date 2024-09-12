5b9097f48ec8ca0775ddfa7bdb91e47adce0e893
Leo Guinan <leo.guinan@gmail.com>
2024-09-11 20:01:16 -0400

    additional logging

--- scripts/app-server.js
+++ scripts/app-server.js
@@ -6,39 +6,41 @@ const app = express();
 const port = process.env.PORT || 3000;
 // Database connection
 const pool = new Pool({
-  connectionString: process.env.DIRECT_DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    connectionString: process.env.DIRECT_DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
 });
 
 app.get('/:slug', async (req, res) => {
-  const { slug } = req.params;
-  try {
-    const result = await pool.query('SELECT "chatUrl" FROM "User" WHERE slug = $1', [slug]);
-    console.log("Number of results", result.rows.length)
-    if (result.rows.length > 0) {
-      const chatUrl = result.rows[0].chatUrl;
-      console.log("Chat URL", chatUrl)
-      res.redirect(301, `${chatUrl}`);
-    } else {
-      res.status(404).send('User not found');
+    console.log("Looking for slug", slug)
+    const { slug } = req.params;
+    try {
+        const result = await pool.query('SELECT "chatUrl" FROM "User" WHERE slug = $1', [slug]);
+        console.log("Number of results", result.rows.length)
+
+        if (result.rows.length > 0) {
+            const chatUrl = result.rows[0].chatUrl;
+            console.log("Chat URL", chatUrl)
+            res.redirect(301, `${chatUrl}`);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (err) {
+        console.error('Database query error:', err);
+        res.status(500).send('Server error');
     }
-  } catch (err) {
-    console.error('Database query error:', err);
-    res.status(500).send('Server error');
-  }
 });
 
 app.get('/', (req, res) => {
-  res.send('Health check');
+    res.send('Health check');
 });
 
 app.listen(port, () => {
-  console.log(`Redirect server running on port ${port}`);
+    console.log(`Redirect server running on port ${port}`);
 });
