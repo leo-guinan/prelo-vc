e93673788ac501fba122449aa82e58d52b63efdf
Leo Guinan <leo.guinan@gmail.com>
2024-10-08 14:14:27 -0400

    fixing stripe issue

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -33,6 +33,7 @@ export async function createCheckoutSession(userId: string) {
   const params: Stripe.Checkout.SessionCreateParams = {
     submit_type: 'pay',
     payment_method_types: ['card'],
+    mode: "subscription",
     line_items: [{ price: process.env.STRIPE_PRICE_ID, quantity: 1 }],
     success_url: `${process.env.NEXT_PUBLIC_APP_URL}/?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/`,
